01d8f39fa067876d3df7c7a8884057ee6779e3b9
Leah H <harrichl@gmail.com>
2021-06-23 00:40:14 -0400

    add initial project files

---
+++ src/main/java/io/lhdev/codinggamechallenge/CodinggamechallengeApplication.java
@@ -0,0 +1,13 @@
+package io.lhdev.codinggamechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodinggamechallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodinggamechallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/lhdev/codinggamechallenge/CodinggamechallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lhdev.codinggamechallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodinggamechallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
